7e04d7dac5f37af6133fe1a8b89d71a61edfd2ff
Lloyd Allen <lkcallen@asu.edu>
2016-04-09 16:45:52 -0700

    Implemented Dragon and DragonLibrary classes. DragonLibrary implements the library via an array list of Dragon, with Dragon values stored within the library.

--- src/dragonspiretournament/GameObjects/Dragons/Dragon.java
+++ src/dragonspiretournament/GameObjects/Dragons/Dragon.java
@@ -2,4 +2,83 @@ package dragonspiretournament.GameObjects.Dragons;
 
 public class Dragon {
 
+    private static final int DEFAULT_DAMAGE = 5;
+    private static final double DEFAULT_MULTIPLIER = 2.0;
+
+    private int damage;
+    private double multiplier;
+    private String name;
+    private String type;
+    private String strength; //Type strong against
+    private String statEffect; //Status effect
+    private String icon; //Location of the image file
+
+    //Default Dragon constructor
+    public Dragon(String dragonName, String dragonType, String typeStrong, String dragonIcon) {
+        name = dragonName;
+        damage = DEFAULT_DAMAGE;
+        multiplier = DEFAULT_MULTIPLIER;
+        type = dragonType;
+        strength = typeStrong;
+        statEffect = "none";
+        icon = dragonIcon;
+    }
+
+    //Overloaded constructor for specifying damage and multiplier
+    public Dragon(String dragonName, int dragonDamage, double damageMultiplier, String dragonType, String typeStrong, String dragonIcon) {
+        name = dragonName;
+        damage = dragonDamage;
+        multiplier = damageMultiplier;
+        type = dragonType;
+        strength = typeStrong;
+        statEffect = "none";
+        icon = dragonIcon;
+    }
+
+    //Default Dragon constructor with status effects
+    public Dragon(String dragonName, String dragonType, String typeStrong, String dragonEffect, String dragonIcon) {
+        name = dragonName;
+        damage = DEFAULT_DAMAGE;
+        multiplier = DEFAULT_MULTIPLIER;
+        type = dragonType;
+        strength = typeStrong;
+        statEffect = dragonEffect;
+        icon = dragonIcon;
+    }
+
+    //Overloaded constructor for status effects
+    public Dragon(String dragonName, int dragonDamage, double damageMultiplier, String dragonType, String typeStrong, String statusEffect, String dragonIcon) {
+        name = dragonName;
+        damage = dragonDamage;
+        multiplier = damageMultiplier;
+        type = dragonType;
+        strength = typeStrong;
+        statEffect = statusEffect;
+        icon = dragonIcon;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public String getStatEffect() {
+        return statEffect;
+    }
+
+    public String getIcon() {
+        return icon;
+    }
+
+    public int attack(String otherType) {
+        int totalDamage = damage;
+
+        if(strength.equals(otherType))
+            totalDamage = (int) (totalDamage * multiplier);
+
+        return totalDamage;
+    }
 }
