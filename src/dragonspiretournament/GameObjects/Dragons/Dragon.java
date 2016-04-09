d383902a2bd38d64d9896dd71988782d7356113f
pin 317 <pin@317>
2016-04-09 15:02:35 -0700

    Advanced ArmyView/Modle impl

--- src/dragonspiretournament/GameObjects/Dragons/Dragon.java
+++ src/dragonspiretournament/GameObjects/Dragons/Dragon.java
@@ -1,5 +1,55 @@
 package dragonspiretournament.GameObjects.Dragons;
 
-public class Dragon {
+import javax.swing.Icon;
+
+// TODO: Auto-generated Javadoc
+/**
+ * The Class Dragon.
+ */
+public abstract class Dragon {
+	
+	/** The dragon name. */
+	private String dragonName;
+	
+	/** The dragon icon. */
+	private Icon dragonIcon;
+	
+	/**
+	 * Gets the dragon name.
+	 *
+	 * @return the dragon name
+	 */
+	public String getDragonName() {
+		return dragonName;
+	}
+	
+	/**
+	 * Sets the dragon name.
+	 *
+	 * @param dragonName the new dragon name
+	 */
+	public void setDragonName(String dragonName) {
+		this.dragonName = dragonName;
+	}
+	
+	/**
+	 * Gets the dragon icon.
+	 *
+	 * @return the dragon icon
+	 */
+	public Icon getDragonIcon() {
+		return dragonIcon;
+	}
+	
+	/**
+	 * Sets the dragon icon.
+	 *
+	 * @param dragonIcon the new dragon icon
+	 */
+	public void setDragonIcon(Icon dragonIcon) {
+		this.dragonIcon = dragonIcon;
+	}
+	
+	
 
 }
---
+++ src/dragonspiretournament/GameObjects/Dragons/TestDragon.java
@@ -0,0 +1,10 @@
+package dragonspiretournament.GameObjects.Dragons;
+
+import javax.swing.ImageIcon;
+
+public class TestDragon extends Dragon {
+	public TestDragon() {
+		super();
+		this.setDragonIcon(new ImageIcon("src/dragons/icons/testDragonIcon.png"));
+	}
+}
