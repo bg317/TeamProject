d619f781e5d9bd52f545317f0617f677c3db00e1
CThompson <cmthom10@asu.edu>
2016-04-10 13:23:48 -0700

    Now pulls the correct value from the entry field of the view and creates Player objects, sending the name to each Player object with a (not yet created) setName method in the Player class.

--- src/dragonspiretournament/nameselection/NameSelectionController.java
+++ src/dragonspiretournament/nameselection/NameSelectionController.java
@@ -3,21 +3,31 @@ package dragonspiretournament.nameselection;
 public class NameSelectionController {
 	
 	static NameSelectionModel nameSelector = new NameSelectionModel();
-	
+	/**
+	 * Gets the name of the current player.
+	 * @return the name of the player grabbed from the model.
+	 */
 	public static String getPlayer()
 	{
 		return nameSelector.getPlayer();
 	}
 	
+	/**
+	 * Sets the name of the current player.
+	 * @param The name to set as the current player's name.
+	 */
 	public static void setName(String name)
 	{
 		nameSelector.setPlayerName(name);
 	}
 	
+	/**
+	 * Determines whether we should get more player names or move on to the game.
+	 * @return a boolean, true when we are done getting names, false when we are not.
+	 */
 	public static boolean goToNextFrame()
 	{
 		return nameSelector.getNext();
 	}
 
 }
-
