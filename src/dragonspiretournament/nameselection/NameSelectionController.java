818ac758f70a1822ed581929e970ece1d6df9425
CThompson <cmthom10@asu.edu>
2016-04-09 23:42:32 -0700

    The name selection screen now does the bare bone basics of what it's supposed to do: it allows both the first and second player to enter a name in sucession, which it then stores. This is working locally, but I isolated it from the rest of the project in order to get it to work in my current Eclipse configuration (and so I could test it by itself). Hopefully it integrates smoothly.

--- src/dragonspiretournament/nameselection/NameSelectionController.java
+++ src/dragonspiretournament/nameselection/NameSelectionController.java
@@ -1,5 +1,23 @@
 package dragonspiretournament.nameselection;
 
 public class NameSelectionController {
+	
+	static NameSelectionModel nameSelector = new NameSelectionModel();
+	
+	public static String getPlayer()
+	{
+		return nameSelector.getPlayer();
+	}
+	
+	public static void setName(String name)
+	{
+		nameSelector.setPlayerName(name);
+	}
+	
+	public static boolean goToNextFrame()
+	{
+		return nameSelector.getNext();
+	}
 
 }
+
--- src/dragonspiretournament/nameselection/NameSelectionModel.java
+++ src/dragonspiretournament/nameselection/NameSelectionModel.java
@@ -1,5 +1,61 @@
 package dragonspiretournament.nameselection;
 
 public class NameSelectionModel {
-
+	
+	//for ease of remembering which player we're dealing with
+	private final int ONE = 0;
+	private final int TWO = 1;
+	
+	 //keeps track of which name is being stored/changed
+	private int currentPlayer;
+	//the stored names
+	private String playerOneName;
+	private String playerTwoName;
+	
+	NameSelectionModel()
+	{
+		currentPlayer = ONE;
+		playerOneName = "Player 1";
+		playerTwoName = "Player 2";
+	}
+	
+	//gets the current player that we are setting
+	public String getPlayer()
+	{
+		if (currentPlayer == ONE)
+		{
+			return playerOneName;
+		}
+		else
+		{
+			return playerTwoName;
+		}
+	}
+	
+	//sets the name of the player
+	public void setPlayerName(String name)
+	{
+		if (currentPlayer == ONE)
+		{
+			playerOneName = name;
+		}
+		else
+		{
+			playerTwoName = name;
+		}
+	}
+	
+	//gets the next place to go--either the second player entry (false) or the start of the game (true)
+	public boolean getNext()
+	{
+		if (currentPlayer == TWO)
+		{
+			return true;
+		}
+		else
+		{
+			currentPlayer++;
+			return false;
+		}
+	}
 }
