38107a8b5d4564b1c721dd6a0a4c7203de3c4d79
lstandage135 <lstandage135@gmail.com>
2016-04-11 12:02:42 -0700

    Larry Added PlayerChangeNotification

--- src/dragonspiretournament/PlayerNotifications/PlayerChangeNotification.java
+++ src/dragonspiretournament/PlayerNotifications/PlayerChangeNotification.java
@@ -1,5 +1,10 @@
 package dragonspiretournament.PlayerNotifications;
 
+/*Notice: this file may be a redundancy with dragonspiretournament.PlayerInformation.PlayerChangeNotification:
+ * 
+ * 
+ */
+
 public class PlayerChangeNotification {
 	
 	//Create a JDialog box for notifying players will be switching on construction
