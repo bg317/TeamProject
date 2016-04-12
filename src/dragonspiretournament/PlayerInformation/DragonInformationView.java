341194add5d6210bf88ce973b596ecf90170a466
CThompson <cmthom10@asu.edu>
2016-04-12 16:08:39 -0700

    Updated dragon information window to work with the rest of the project correctly. Utilizes a new object ImageButton, similar to the DragonButton, but can be used for an image that doesn't want/need to have more information about the dragon other than the path of the image.

--- src/dragonspiretournament/PlayerInformation/DragonInformationView.java
+++ src/dragonspiretournament/PlayerInformation/DragonInformationView.java
@@ -1,9 +1,14 @@
 package dragonspiretournament.PlayerInformation;
+import dragonspiretournament.GameObjects.Dragons.Dragon;
+import dragonspiretournament.GameObjects.UIComponents.ImageButton;
 
 import java.awt.BorderLayout;
+import java.awt.GridLayout;
+import java.awt.Image;
+import java.awt.Graphics;
 import java.awt.image.BufferedImage;
 import java.io.IOException;
-
+import java.io.File;
 import javax.imageio.ImageIO;
 import javax.swing.ImageIcon;
 import javax.swing.JButton;
@@ -19,6 +24,7 @@ public class DragonInformationView {
 	private String dragonIconLocation;
 	private String dragonArtLocation;
 	private String dragonElement;
+	private String dragonAdvantage;
 	
 	
 	private JFrame infoFrame;
@@ -27,9 +33,12 @@ public class DragonInformationView {
 	private JPanel totalPanel;
 	private JLabel nameLabel;
 	private JLabel descriptionLabel;
+	private JPanel statsPanel;
+	private JLabel elementLabel;
+	private JPanel descriptionPanel;
 	private JButton closeButton;
 	private ImageIcon icon;
-	private JLabel mainImageLabel;
+	private ImageButton mainImage;
 
 	
 	//constructor, takes in dragon information when created
@@ -43,10 +52,13 @@ public class DragonInformationView {
 		dragonDescription = DragonInformationController.getDescription();
 		dragonIconLocation = DragonInformationController.getIcon();
 		dragonArtLocation = DragonInformationController.getArt();
+		dragonAdvantage = DragonInformationController.getAdvantage();
 		
 		
 		//creating a frame
 		infoFrame = new JFrame("Dragonspire Tournament--" + dragonName + " Information");
+		infoFrame.getContentPane().setLayout(new GridLayout(1,1));
+		infoFrame.setSize(500, 600);
 		
 		//initializing images
 		
@@ -56,31 +68,34 @@ public class DragonInformationView {
 		//initializing the J elements
 		totalPanel = new JPanel(new BorderLayout());
 		nameLabel = new JLabel("<html><h1 style='font-size:25px; color: #990000; padding: 10px'>"+dragonName+"</h1></html>", JLabel.CENTER);
-		descriptionLabel = new JLabel("<html><p style='font-size:12px; weight: normal; #595959; padding: 10px'>"+dragonDescription+" <br/>Element: " + dragonElement + "</p></html>");
+		descriptionLabel = new JLabel("<html><table style='font-size:12px; weight: normal; #595959; padding: 10px'>"+dragonDescription +"</html>");
+		descriptionPanel = new JPanel(new GridLayout(2,1));
+		descriptionPanel.add(descriptionLabel);
+		statsPanel = new JPanel(new GridLayout(1,2));
+		elementLabel = new JLabel("Element: " + dragonElement);
+		statsPanel.add(elementLabel);
+		
+		descriptionPanel.add(statsPanel);
 		
 		//adding the J elements to the total panel
 		totalPanel.add(nameLabel, BorderLayout.NORTH);
-		totalPanel.add(descriptionLabel, BorderLayout.SOUTH);
+		totalPanel.add(descriptionPanel, BorderLayout.SOUTH);
 		
 		//set main dragon image
-		BufferedImage mainImage;
 		try {
 			System.out.println(dragonArtLocation);
-			mainImage = ImageIO.read(this.getClass().getResource(dragonArtLocation));
-			mainImageLabel = new JLabel(new ImageIcon(mainImage));
-			totalPanel.add(mainImageLabel, BorderLayout.CENTER);
-		} 
-		catch (IOException e) {
-			System.out.println("error, invalid file");
+			mainImage = new ImageButton(dragonArtLocation);
+			totalPanel.add(mainImage, BorderLayout.CENTER);
+			System.out.println("got to the end of the try");
 		}
-
+		finally
+		{
 		
-		//adds functionality to the button
-
+		System.out.println("doing finally stuff");
 		//frame stuff
 		infoFrame.getContentPane().add(totalPanel);
-		infoFrame.setSize(500, 500);
 		infoFrame.setVisible(true);
+		}
 	}
 	
 }
