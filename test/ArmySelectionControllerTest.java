f4ba9b3cbfd42ff185fa518ba0abfcfb49888ffa
lstandage135 <lstandage135@gmail.com>
2016-04-13 13:02:34 -0700

    larry added empty tests for ArmySelectController MatchModel and MatchController

---
+++ test/ArmySelectionControllerTest.java
@@ -0,0 +1,34 @@
+
+import dragonspiretournament.armyselection.ArmySelectionController;
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class ArmySelectionControllerTest {
+
+	@Test
+	public void testInitCurrentAndPrev() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testRemoveFromArmy() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testAddToArmy() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testShiftPreviewLeft() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testShiftPreviewRight() {
+		fail("Not yet implemented");
+	}
+
+}
