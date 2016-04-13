b2405039e803b3dd9997f7e1b8e77037b537cfca
Brandon Gardell <bgardell@email.asu.edu>
2016-04-13 09:40:17 -0700

    Test files should be located in the test dir

---
+++ test/TitleControllerTest.java
@@ -0,0 +1,41 @@
+/* TitleViewController Test
+ * 
+ * @author larry
+ * @version 4-12-16
+ * 
+ */
+package dragonspiretournament.title;
+
+import static org.junit.Assert.*;
+
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+public class TitleControllerTest {
+
+	private final ByteArrayOutputStream outContent = new ByteArrayOutputStream();
+	private final ByteArrayOutputStream errContent = new ByteArrayOutputStream();
+
+	@Before
+	public void setUpStreams() {
+	    System.setOut(new PrintStream(outContent));
+	    System.setErr(new PrintStream(errContent));
+	}
+
+	@After
+	public void cleanUpStreams() {
+	    System.setOut(null);
+	    System.setErr(null);
+	}
+	
+	@Test
+	public void doingSomethingTest() {
+		TitleController.doSomething();
+		assertEquals("Doing Something\n", outContent.toString());
+	}
+
+}
