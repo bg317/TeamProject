a9700bf5cc94435bca3dde52ae69dfd0689920ba
lstandage135 <lstandage135@gmail.com>
2016-04-13 11:31:26 -0700

    larry added a preliminary ArmySelectionModelTest

---
+++ test/ArmySelectionModelTest.java
@@ -0,0 +1,84 @@
+package dragonspiretournament.armyselection;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class ArmySelectionModelTest {
+
+	@Test
+	public void testGetCurrentPlayerArmy() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSetCurrentPlayerArmy() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testArmySelectionModelPlayerDragonLibrary() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testArmySelectionModel() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetPlayer() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSetPlayer() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetAllLibraryDragons() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetDragLib() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSetDragLib() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetCurrent() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSetCurrent() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetPrev() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSetPrev() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetNext() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSetNext() {
+		fail("Not yet implemented");
+	}
+
+}
