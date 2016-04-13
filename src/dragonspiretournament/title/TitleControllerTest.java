b01880610150d5271d91caf55641f81e1654a1a6
lstandage135 <lstandage135@gmail.com>
2016-04-12 23:05:31 -0700

    larry added TitleControllerTest.java

---
+++ src/dragonspiretournament/title/TitleControllerTest.java
@@ -0,0 +1,35 @@
+package dragonspiretournament.title;
+
+import static org.junit.Assert.*;
+
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+public class TitleControllerTest {
+
+	private final ByteArrayOutputStream outContent = new ByteArrayOutputStream();
+	private final ByteArrayOutputStream errContent = new ByteArrayOutputStream();
+
+	@Before
+	public void setUpStreams() {
+	    System.setOut(new PrintStream(outContent));
+	    System.setErr(new PrintStream(errContent));
+	}
+
+	@After
+	public void cleanUpStreams() {
+	    System.setOut(null);
+	    System.setErr(null);
+	}
+	
+	@Test
+	public void doingSomethingTest() {
+		TitleController.doSomething();
+		assertEquals("Doing Something\n", outContent.toString());
+	}
+
+}
