0c157077d53eca71e5e7e508d90719f9ac763f36
lstandage135 <lstandage135@gmail.com>
2016-04-13 13:02:34 -0700

    larry added empty tests for ArmySelectController MatchModel and MatchController

---
+++ test/MatchControllerTest.java
@@ -0,0 +1,60 @@
+
+import dragonspiretournament.match.MatchController;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class MatchControllerTest {
+
+	@Test
+	public void testDoDragonAttack() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testUpdatePlayerStrongholdHP() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testUpdatePlayerDice() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testDiceRoll() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testAddToDiceSelection() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testUpdateDragonToAdd() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testUpdateDragonToDelete() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testRemoveFromDiceSelection() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSetPlayerOneDice() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSetPlayerTwoDice() {
+		fail("Not yet implemented");
+	}
+
+}
---
+++ test/MatchModelTest.java
@@ -0,0 +1,150 @@
+
+import dragonspiretournament.match.MatchModel;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class MatchModelTest {
+
+	@Test
+	public void testGetPlayerOneLastAction() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testMatchModelPlayerPlayer() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testMatchModel() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetPlayerOne() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSetPlayerOne() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetPlayerTwo() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSetPlayerTwo() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetPlayerOneArmy() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSetPlayerOneArmy() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetPlayerTwoArmy() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSetPlayerTwoArmy() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetPlayerOneDice() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSetPlayerOneDice() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetPlayerTwoDice() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSetPlayerTwoDice() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSetPlayerOneLastAction() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetPlayerTwoLastAction() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSetPlayerTwoLastAction() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetPlayerOneLastDragon() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSetPlayerOneLastDragon() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetPlayerTwoLastDragon() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSetPlayerTwoLastDragon() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetCurrentDiceSelection() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSetCurrentDiceSelection() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetDragonBeingAddedToDice() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSetDragonBeingAddedToDice() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetDragonBeingRemovedFromDice() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSetDragonBeingRemovedFromDice() {
+		fail("Not yet implemented");
+	}
+
+}
