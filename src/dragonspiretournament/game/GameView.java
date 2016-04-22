488a09ef88fb02efe242bc64e2b63cd6fb11dd85
CThompson <cmthom10@asu.edu>
2016-04-22 15:38:18 -0700

    GameView now can create a NameSelectionView that shows up correctly.

--- src/dragonspiretournament/game/GameView.java
+++ src/dragonspiretournament/game/GameView.java
@@ -4,6 +4,7 @@ import dragonspiretournament.GameObjects.Dragons.Dragon;
 
 import dragonspiretournament.GameObjects.UIComponents.ImageButton;
 import dragonspiretournament.title.TitleController;
+import dragonspiretournament.nameselection.NameSelectionView;
 
 import java.awt.BorderLayout;
 import java.awt.CardLayout;
@@ -29,6 +30,9 @@ public class GameView {
 	/** The title frame. */
 	private JFrame titleFrame;
 	
+	/*test stuff*/
+	private JButton testButton;
+	
 	/**
 	 * Instantiates a new game view.
 	 */
@@ -36,6 +40,19 @@ public class GameView {
 		titleFrame = new JFrame("DragonSpire Tournament");
 		titleFrame.setSize(1036, 610);
 		
+		titleFrame.setVisible(true);
+		
+		//setting the card layout
+		titleFrame.getContentPane().setLayout(new CardLayout(20,20));
+		
+		//creating elements to add to the card layout
+		NameSelectionView nameView = new NameSelectionView();
+		
+		//adding elements to the card layout
+		//test adds
+		//testButton = new JButton("test");
+		//titleFrame.getContentPane().add(testButton);
+		titleFrame.getContentPane().add(nameView);
 		titleFrame.setVisible(true);
 	}
 
